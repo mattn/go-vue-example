f572d2f131a14d9f52c60036c588635ba7218ad1
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-03-30 11:11:31 +0900

    use arrow function, possibly

--- static/app.js
+++ static/app.js
@@ -1,44 +1,42 @@
 ELEMENT.locale(ELEMENT.lang.ja)
 var app = new Vue({
   el: '#app',
-  ready: {
-  },
   data: {
     tasks: [],
     newTask: "",
     loading: false,
   },
   created: function() {
-    this.loading = true;
+    this.$data.loading = true;
     axios.get('/tasks')
-      .then(function (response) {
+      .then((response) => {
         console.log(response);
         app.$data.tasks = response.data.items;
         app.$data.loading = false;
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.log(error);
         app.$data.loading = false;
       });
   },
   methods: {
-    addTask: function(task) {
+    addTask: (task) => {
       app.$data.loading = true;
       let params = new URLSearchParams();
       params.append('body', app.$data.newTask);
       axios.post('/tasks', params)
-        .then(function (response) {
+        .then((response) => {
           app.$data.loading = false;
           app.$data.tasks.push(response.data);
           app.$data.newTask = "";
           app.$data.loading = false;
         })
-        .catch(function (error) {
+        .catch((error) => {
           console.log(error);
           app.$data.loading = false;
         });
     },
-    doneTask: function(task) {
+    doneTask: (task) => {
       app.$data.loading = true;
       let params = new URLSearchParams();
       params.append('done', !task.done);
@@ -47,7 +45,7 @@ var app = new Vue({
           console.log(response);
           app.$data.loading = false;
         })
-        .catch(function (error) {
+        .catch((error) => {
           console.log(error);
           app.$data.loading = false;
         });
