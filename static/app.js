7b2cd24e4c6aab0e68013757e79dee4a4f42a951
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-09-12 17:33:59 +0900

    add removing item

--- static/app.js
+++ static/app.js
@@ -7,50 +7,65 @@ var app = new Vue({
     loading: false,
   },
   created: function() {
-    this.loading = true;
+    this.loading = true
     axios.get('/tasks')
       .then((response) => {
-        console.log(response);
-        this.tasks = response.data.items || [];
-        this.loading = false;
+        console.log(response)
+        this.tasks = response.data.items || []
+        this.loading = false
       })
       .catch((error) => {
-        console.log(error);
-        this.loading = false;
-      });
+        console.log(error)
+        this.loading = false
+      })
   },
   methods: {
     addTask: function(task) {
-      this.loading = true;
-      let params = new URLSearchParams();
-      params.append('body', this.newTask);
-      params.append('done', false);
+      this.loading = true
+      let params = new URLSearchParams()
+      params.append('body', this.newTask)
+      params.append('done', false)
       axios.post('/tasks', params)
         .then((response) => {
-          this.loading = false;
-          this.tasks.unshift(response.data);
-          this.newTask = "";
-          vue.$forceUpdate();
-          this.loading = false;
+          this.loading = false
+          this.tasks.unshift(response.data)
+          this.newTask = ""
+          vue.$forceUpdate()
+          this.loading = false
         })
         .catch((error) => {
-          console.log(error);
-          this.loading = false;
-        });
+          console.log(error)
+          this.loading = false
+        })
     },
     doneTask: function(task) {
-      this.loading = true;
-      let params = new URLSearchParams();
-      params.append('done', !task.done);
+      this.loading = true
+      let params = new URLSearchParams()
+      params.append('done', !task.done)
       axios.put('/tasks/' + task.id, params)
         .then((response) => {
-          console.log(response);
-          this.loading = false;
+          console.log(response)
+          this.loading = false
         })
         .catch((error) => {
-          console.log(error);
-          this.loading = false;
-        });
-    } 
+          console.log(error)
+          this.loading = false
+        })
+    },
+    removeTask: function(task) {
+      this.loading = true
+      axios.delete('/tasks/' + task.id)
+        .then((response) => {
+          console.log(response)
+          Vue.delete(this.tasks, task)
+          this.tasks.forEach(function(v, i, a) { if (v.id == task.id) a.splice(i,1) })
+          vue.$forceUpdate()
+          this.loading = false
+        })
+        .catch((error) => {
+          console.log(error)
+          this.loading = false
+        })
+    }
   }
 })
